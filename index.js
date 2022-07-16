0246480071f12455302265d9c03c47cf8fa67bca
yoshiaki <yoshiosmile68@gmail.com>
2022-07-16 17:24:54 +0900

    変更

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import https from "https"
-import express from "express"
+const https = require( "https")
+const express = require( "express")
 const app = express()
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
