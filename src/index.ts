df66b3202646f876dcb96214e01a19e99fb75b6a
yoshiaki <yoshiosmile68@gmail.com>
2022-07-18 02:40:05 +0900

    あ

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,18 @@
 import express from "express";
 
 import { middlewareConfig } from "./lib/client";
-import { handleEvent } from "./lib/handleEvent";
+// import { handleEvent } from "./lib/handleEvent";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (_, res: { sendStatus: (arg0: number) => void }) => {
-  res.sendStatus(200);
+app.get("/", (_, res) => {
+  res.status(200).json("okke");
 });
 
 app.post("/webhook", middlewareConfig, (req, res) => {
-  const events = req.body.events;
-  console.log("------------", events);
-  Promise.all(events.map(handleEvent)).then((result) => res.json(result));
+  // const events = req.body.events;
+  // Promise.all(events.map(handleEvent)).then((result) => res.json(result));
   res.sendStatus(200);
 });
 
