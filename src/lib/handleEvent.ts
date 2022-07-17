9cbc51b3110f20ea5cf2e28d858cc2a19ef4536f
yoshiaki <yoshiosmile68@gmail.com>
2022-07-18 02:18:26 +0900

    エラー処理

--- src/lib/handleEvent.ts
+++ src/lib/handleEvent.ts
@@ -4,18 +4,19 @@ import { reservationRes } from "./message/reservationRes";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handleEvent = (event: any) => {
-  if (event.type === "message") {
-    const messages: Message[] = [];
-
-    if (event.message.text === "予約") {
-      messages.push(reservationRes);
-    } else {
-      messages.push({
-        type: "text",
-        text: "何かお困りごとはございますか?",
-      } as TextMessage);
-    }
-
-    return client.replyMessage(event.replyToken, messages);
+  if (event.type !== "message" || event.message.type !== "text") {
+    return Promise.resolve(null);
   }
+  const messages: Message[] = [];
+
+  if (event.message.text === "予約") {
+    messages.push(reservationRes);
+  } else {
+    messages.push({
+      type: "text",
+      text: "何かお困りごとはございますか?",
+    } as TextMessage);
+  }
+
+  return client.replyMessage(event.replyToken, messages);
 };
