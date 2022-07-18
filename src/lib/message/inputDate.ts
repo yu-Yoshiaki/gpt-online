68fbe7166a90d05c97c56d0fdd2f479fd3f76130
yoshiaki <yoshiosmile68@gmail.com>
2022-07-18 16:44:53 +0900

    予約日を決める

--- src/lib/message/inputDate.ts
+++ src/lib/message/inputDate.ts
@@ -1,6 +1,21 @@
-import { TextMessage } from "@line/bot-sdk";
+import { Message } from "@line/bot-sdk";
 
-export const inputDate: TextMessage = {
-  type: "text",
-  text: "予約日を決める",
-};
+export const inputDate: Message[] = [
+  {
+    type: "template",
+    altText: "This is a buttons template",
+    template: {
+      type: "buttons",
+      title: "予約日を決めてください。",
+      text: "Please select",
+      actions: [
+        {
+          type: "datetimepicker",
+          mode: "date",
+          label: "予約日を決めきめる。",
+          data: "action=reserve&date=confirm",
+        },
+      ],
+    },
+  },
+];
