d841b148bf9e13bd5d1502e1fbdb743490d7b666
yoshiaki <yoshiosmile68@gmail.com>
2022-08-01 14:29:07 +0900

    kaizen

--- src/lib/handleEvent.ts
+++ src/lib/handleEvent.ts
@@ -1,8 +1,6 @@
 import { client } from "./client";
-import { createRichMenu, setDefaultMenu, setRichMenuImage } from "./createRichMenu";
 import { followMessage } from "./message/follow";
 import { confirmDate, inputDate } from "./message/inputDate";
-import { richMenuData } from "./message/richMenuData";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const handleEvent = (event: any) => {
@@ -17,19 +15,6 @@ export const handleEvent = (event: any) => {
       client.getRichMenuList().then((menu) => {
         console.log("++++++++++++=", menu);
       });
-    } else if (eventText === "リッチメニュー作成") {
-      createRichMenu(richMenuData)
-        .then((id) => {
-          console.log("id---", id);
-          setRichMenuImage({
-            richMenuId: id,
-            url: "https://images.microcms-assets.io/assets/47e7fcaea43947baa0cd0b9e6f59b2f9/abe8185c24904d0dac93541bb3cf64aa/7E3A256F-ED0F-488E-AC74-7FDDA076F515.png",
-          });
-          setDefaultMenu(id);
-        })
-        .catch((err) => {
-          console.log("error", err);
-        });
     } else {
       return client.replyMessage(event.replyToken, {
         type: "text",
@@ -51,6 +36,8 @@ export const handleEvent = (event: any) => {
       const messages = confirmDate(event);
       return client.replyMessage(event.replyToken, messages);
     } else if (event.postback.data === "action=reserve&date=retry") {
+      return client.replyMessage(event.replyToken, inputDate);
+    } else if (event.postback.data === "action=buy&itemid=123") {
       return client.replyMessage(event.replyToken, inputDate);
     }
   }
