575fd42b9a5392e61b7c41453e991084a43a7a4a
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-08-29 15:32:04 +0900

    kaizen

---
+++ src/client.ts
@@ -0,0 +1,10 @@
+import { Client, middleware } from "@line/bot-sdk";
+
+// const channelAccessToken = process.env.LINE_ACCESS_TOKEN as string;
+// const channelSecret = process.env.LINE_CHANNEL_SECRET as string;
+
+const channelAccessToken = process.env.LINE_ACCESS_TOKEN as string;
+const channelSecret = process.env.LINE_CHANNEL_SECRET as string;
+
+export const client = new Client({ channelAccessToken });
+export const middlewareConfig = middleware({ channelSecret });
