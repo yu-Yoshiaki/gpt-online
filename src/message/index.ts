818f8aa4ffe7b709a3f7b5f0c97508ec1d8d90da
yumotoyoshiaki <yoshiosmile68@gmail.com>
2022-09-06 16:52:48 +0900

    kaizen

--- src/message/index.ts
+++ src/message/index.ts
@@ -4,23 +4,37 @@ import { fetchMyReservation } from "../fetchMyReservation";
 import { fetchUserInfo } from "../fetchUserInfo";
 import { fetchVacancy } from "../fetchVacancy";
 import { redirectSignupMessage } from "./redirectSignupMessage";
+import { Message } from "@line/bot-sdk";
 
-export const message = (event: any) => {
+export const message = async (event: any) => {
   const eventText = event.message.text;
 
-  if (eventText === "予約" || /予約(する|したい|申し込[みむ])/.test(eventText)) {
-    fetchUserInfo(event.source.userId).then((data) => {
-      if (!data) return client.replyMessage(event.replyToken, redirectSignupMessage);
-      return client.replyMessage(event.replyToken, fetchVacancy());
-    });
-  } else if (eventText === "予約確認") {
-    fetchMyReservation(event.source.userId).then((message) => {
-      return client.replyMessage(event.replyToken, message);
-    });
-  } else {
-    return client.replyMessage(event.replyToken, {
-      type: "text",
-      text: "何かお困りごとはございませんか？",
-    });
-  }
+  const message = async () => {
+    const replyM: Message[] = [];
+    if (eventText === "予約" || /予約(する|したい|申し込[みむ])/.test(eventText)) {
+      const data = await fetchUserInfo(event.source.userId);
+      if (!data) {
+        replyM.push(...redirectSignupMessage);
+      } else {
+        replyM.push(fetchVacancy());
+      }
+    }
+
+    if (eventText === "予約確認") {
+      const data = await fetchMyReservation(event.source.userId);
+      replyM.push(...data);
+    }
+
+    return replyM;
+  };
+
+  message().then((message) => {
+    if (message.length === 0) {
+      return client.replyMessage(event.replyToken, {
+        type: "text",
+        text: "何かお困りごとはございませんか？",
+      });
+    }
+    return client.replyMessage(event.replyToken, message);
+  });
 };
